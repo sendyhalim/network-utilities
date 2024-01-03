6f29656f452b43edbb45ae6e3845c65e56d9ab99
Sendy Halim <sendyhalim93@gmail.com>
2024-01-03 22:55:30 +0700

    refactor: Rename to traceroute-poc-raw-socket

--- src/main/traceroute-poc-raw-socket.rs
+++ src/main/traceroute-poc-raw-socket.rs
@@ -1,11 +1,9 @@
-use socket2::{Domain, SockAddr, Socket};
+use socket2::Domain;
+use socket2::Socket;
 use std::mem::MaybeUninit;
 use std::net::IpAddr;
 use std::net::Ipv4Addr;
 use std::net::SocketAddr;
-use std::net::SocketAddrV4;
-use std::net::UdpSocket;
-use std::os::fd::AsRawFd;
 use std::time::Duration;
 
 fn main() -> std::io::Result<()> {
@@ -16,6 +14,7 @@ fn main() -> std::io::Result<()> {
     .collect::<Vec<_>>();
 
   let ip = ips.first().unwrap();
+  // let ip: IpAddr = "192.168.1.1".parse().unwrap();
 
   println!("Going to connect to {}", ip);
 
@@ -24,20 +23,18 @@ fn main() -> std::io::Result<()> {
   let udp_socket_addr_client: SocketAddr;
   let udp_socket_addr_dest: SocketAddr;
 
-  let icmp_socket: Socket;
-  let udp_socket: UdpSocket;
-  let hop_limit: u32 = 5;
+  let hop_limit: u32 = 9;
 
   // Preparing the IPV4 ICMP socket
   // ----------------------------------------
-  icmp_socket = Socket::new(
+  let icmp_socket = Socket::new(
     Domain::IPV4,
     socket2::Type::RAW,
     Some(socket2::Protocol::ICMPV4),
   )
   .unwrap();
 
-  // Preparing the IPV4 UDP socket
+  // Preparing the IPV4 UDP socket address
   // ----------------------------------------
   udp_socket_addr_client = format!("0.0.0.0:{}", udp_socket_client_port)
     .parse()
@@ -46,8 +43,62 @@ fn main() -> std::io::Result<()> {
     .parse()
     .unwrap();
 
-  udp_socket = UdpSocket::bind(udp_socket_addr_client).unwrap();
-  udp_socket.set_ttl(hop_limit).unwrap();
+  // Preparing ip raw socket
+  // ----------------------------------------
+  let mut ip_raw_socket = Socket::new(
+    Domain::IPV4,
+    socket2::Type::RAW,
+    Some(socket2::Protocol::UDP),
+  )
+  .unwrap();
+
+  // Prepare the IPv4 payload containing UDP packet
+  // ----------------------------------------
+  let udp_payload: [u8; 2] = [0, 0];
+  let udp_header = etherparse::UdpHeader::without_ipv4_checksum(
+    udp_socket_client_port,
+    udp_socket_dest_port,
+    udp_payload.len(), // Later will be sum-med with udp_header len
+  )
+  .unwrap();
+
+  let mut udp_packet: Vec<u8> = vec![];
+  udp_packet.extend_from_slice(&udp_header.to_bytes());
+  udp_packet.extend_from_slice(&udp_payload);
+
+  let ipv4_addr: Ipv4Addr = udp_socket_addr_dest.ip().to_string().parse().unwrap();
+
+  let mut ipv4_header = etherparse::Ipv4Header::new(
+    udp_packet.len() as u16,
+    hop_limit as u8,
+    17,
+    udp_socket_addr_client
+      .ip()
+      .to_string()
+      .parse::<Ipv4Addr>()
+      .unwrap()
+      .octets(),
+    ipv4_addr.octets(),
+  );
+
+  ipv4_header.identification = 28182;
+
+  // We want to set ip header manually.
+  ip_raw_socket.set_header_included(true).unwrap();
+
+  // Construct raw ip payload
+  let mut raw_ip_payload: Vec<u8> = vec![];
+
+  ipv4_header.write(&mut raw_ip_payload).unwrap();
+  raw_ip_payload.extend_from_slice(&udp_packet);
+
+  println!(
+    "Debug IP header {:#?}\n\nIP bytes length: {}\n\nUDP: {:#?}\n\nUDP payload {:?}",
+    etherparse::Ipv4Header::from_slice(&raw_ip_payload[..20]).unwrap(),
+    ipv4_header.total_len(),
+    etherparse::UdpHeader::from_slice(&raw_ip_payload[20..28]).unwrap(),
+    &raw_ip_payload[28..30]
+  );
 
   // Start ICMP listener first before sending the probes
   // ----------------------------------------
@@ -72,14 +123,10 @@ fn main() -> std::io::Result<()> {
           let icmpv4_body = etherparse::Icmpv4Slice::from_slice(&res_bytes[20..]).unwrap();
 
           println!(
-            "Got some response bro size: {}\nipv4 raw resp: {:?}\nip address:{}\nICMP type: {:?}, ICMP code: {}",
+            "Got some response size: {}\nipv4 raw resp: {:?}\nip address:{}\nICMP type: {:?}, ICMP code: {}",
             size,
             ipv4_resp,
-            if addr.is_ipv4() {
-              addr.as_socket_ipv4().unwrap().to_string()
-            } else {
-              addr.as_socket_ipv6().unwrap().to_string()
-            },
+            addr.as_socket_ipv4().unwrap().to_string(),
             icmpv4_body.icmp_type(),
             icmpv4_body.code_u8()
           );
@@ -91,20 +138,17 @@ fn main() -> std::io::Result<()> {
     }
   });
 
-  // Sending the UDP probes
+  // Send the probe
   // ----------------------------------------
-  let udp_payload: [u8; 2] = [0, 0];
-
-  println!("Sending {:?} {:?}", udp_payload, udp_socket_addr_dest);
-  let sent_count = udp_socket
-    .send_to(&udp_payload, udp_socket_addr_dest)
+  let sent_count = ip_raw_socket
+    .send_to(&raw_ip_payload, &udp_socket_addr_dest.into())
     .unwrap();
 
   println!("Sent {} bytes of UDP", sent_count);
 
-  // Block for icmp listener;
+  // Block the icmp listener
   // ----------------------------------------
-  icmp_listener_handle.join().unwrap();
+  let _ = icmp_listener_handle.join().unwrap();
 
   return Ok(());
 }
